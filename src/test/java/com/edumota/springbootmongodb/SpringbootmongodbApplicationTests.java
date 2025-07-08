be0003c0768e2cb185749673bdcbf88a78f7cc45
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-08 18:05:23 -0300

    First commit

---
+++ src/test/java/com/edumota/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumota.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
