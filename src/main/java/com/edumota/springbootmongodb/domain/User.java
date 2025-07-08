45a6577f96407b39d72519342f9a106e42c0a819
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-08 18:24:31 -0300

    feat: add Entity User and REST working

---
+++ src/main/java/com/edumota/springbootmongodb/domain/User.java
@@ -0,0 +1,36 @@
+package com.edumota.springbootmongodb.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
