917e6d6fb417db81687c119c0964d5e146ed4556
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-09 14:34:24 -0300

    feat: add User projection using DTO

--- src/main/java/com/edumota/springbootmongodb/domain/Post.java
+++ src/main/java/com/edumota/springbootmongodb/domain/Post.java
@@ -1,5 +1,6 @@
 package com.edumota.springbootmongodb.domain;
 
+import com.edumota.springbootmongodb.dto.AuthorDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,7 +28,7 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/java/com/edumota/springbootmongodb/dto/AuthorDTO.java
@@ -0,0 +1,26 @@
+package com.edumota.springbootmongodb.dto;
+
+import com.edumota.springbootmongodb.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class AuthorDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+}
