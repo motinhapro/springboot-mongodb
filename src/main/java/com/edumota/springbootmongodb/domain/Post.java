12b76e7b2286f16b976600bf3b4a14b123ce4d10
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-09 15:16:28 -0300

    feat: add comments to Post

--- src/main/java/com/edumota/springbootmongodb/domain/Post.java
+++ src/main/java/com/edumota/springbootmongodb/domain/Post.java
@@ -1,6 +1,7 @@
 package com.edumota.springbootmongodb.domain;
 
 import com.edumota.springbootmongodb.dto.AuthorDTO;
+import com.edumota.springbootmongodb.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Document
 
 public class Post implements Serializable {
@@ -29,6 +31,16 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 
     @Override
     public boolean equals(Object o) {
