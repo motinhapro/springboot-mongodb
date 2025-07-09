fc433497e72642452aa3da560449ca9829bd1b9f
Carlos Eduardo <carloseduardomotapro@gmail.com>
2025-07-09 16:12:15 -0300

    feat: add query methods to search posts by title, body, comments, and date

--- src/main/java/com/edumota/springbootmongodb/services/PostService.java
+++ src/main/java/com/edumota/springbootmongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.edumota.springbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,6 +21,11 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repository.findByTitleContainingIgnoreCase(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fulLSearch(text, minDate, maxDate);
     }
 
 }
